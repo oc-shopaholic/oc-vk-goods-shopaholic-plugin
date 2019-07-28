dfccbe44f77d738b5892cfa362c210d882f6a7a0
Pavel Lovtsevich <p.lovtsevich@lovata.com>
2019-07-28 09:51:22 +0300

    New translations lang.php (Russian)

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -1,33 +1,33 @@
 <?php return [
     'plugin'     => [
         'name'        => 'VK товары для Shopaholic',
-        'description' => 'Export catalog to VK Goods with using API',
+        'description' => 'Интеграция с API VK Goods для выгрузки каталога',
     ],
     'menu'       => [
-        'settings'             => 'Export to VK Goods',
-        'settings_description' => 'Configure export catalog to VK Goods',
+        'settings'             => 'Экспорт в Товары VK',
+        'settings_description' => 'Настроить экспорт каталога в Товары VK',
     ],
     'field'      => [
-        'community_id'                               => 'Идентификатор сообщества',
-        'code_model_for_images'                      => 'Получать изображения из:?',
+        'community_id'                               => 'ID сообщества',
+        'code_model_for_images'                      => 'Получать изображения из:',
         'section_management_additional_fields_offer' => 'Дополнительные поля',
-        'section_vk_goods'                           => 'VK Goods',
-        'active_vk'                                  => 'Export to VK Goods',
+        'section_vk_goods'                           => 'Товары VK',
+        'active_vk'                                  => 'Экспорт в Товары VK',
         'access_token_user'                          => 'Ключ доступа пользователя',
-        'category_vk_id'                             => 'Category VK Goods',
+        'category_vk_id'                             => 'Категория VK Goods',
         'queue_name'                                 => 'Название очереди',
     ],
     'button'     => [
-        'export_catalog_to_api' => 'Запуск экспорта',
+        'export_catalog_to_api' => 'Начать экспортирование',
     ],
     'widget'     => [
-        'export_catalog_to_api_for_vk_goods' => 'Export catalog to VK Goods',
+        'export_catalog_to_api_for_vk_goods' => 'Экспорт каталога в API (VK Goods)',
     ],
     'permission' => [
-        'vkgoodssettings' => 'Manager settings of catalog export to VK Goods',
+        'vkgoodssettings' => 'Управление настройками экспорта в VK Goods',
     ],
     'message'    => [
-        'export_is_completed'           => 'Export is completed',
-        'update_catalog_to_xml_confirm' => 'Запустить обновление каталога?',
+        'export_is_completed'           => 'Экспортирование начато',
+        'update_catalog_to_xml_confirm' => 'Начать экспортирование каталога?',
     ],
 ];
