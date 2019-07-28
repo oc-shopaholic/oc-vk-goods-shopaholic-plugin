4cfe66845bdcd564d7f99373d29fdcf64266b8f8
Pavel Lovtsevich <p.lovtsevich@lovata.com>
2019-07-28 09:52:23 +0300

    New translations lang.php (Portuguese, Brazilian)

--- lang/pt/lang.php
+++ lang/pt/lang.php
@@ -1,33 +1,33 @@
 <?php return [
     'plugin'     => [
         'name'        => 'VK Goods for Shopaholic',
-        'description' => 'Export catalog to VKontakte with using API',
+        'description' => 'Export catalog to VK Goods using API',
     ],
     'menu'       => [
-        'settings'             => 'Export to VKontakte',
-        'settings_description' => 'Configure export catalog to VKontakte',
+        'settings'             => 'Export to VK Goods',
+        'settings_description' => 'Configure catalog export to VK Goods',
     ],
     'field'      => [
-        'community_id'                               => 'Community id',
+        'community_id'                               => 'Community ID',
         'code_model_for_images'                      => 'Get images from:',
         'section_management_additional_fields_offer' => 'Additional fields',
-        'section_vkontakte'                          => 'VKontakte',
-        'active_vk'                                  => 'Export to VKontakte',
+        'section_vk_goods'                           => 'VK Goods',
+        'active_vk'                                  => 'Export to VK Goods',
         'access_token_user'                          => 'User access key',
-        'category_vk_id'                             => 'Category VKontakte',
+        'category_vk_id'                             => 'VK Goods category',
         'queue_name'                                 => 'Queue name',
     ],
     'button'     => [
         'export_catalog_to_api' => 'Run export',
     ],
     'widget'     => [
-        'export_catalog_to_api_for_vkontakte' => 'Export catalog to Vkontakte',
+        'export_catalog_to_api_for_vk_goods' => 'Export catalog to VK Goods',
     ],
     'permission' => [
-        'vkgoodssettings' => 'Manager settings of catalog export to VKontakte',
+        'vkgoodssettings' => 'Manage settings of catalog export to VK Goods',
     ],
     'message'    => [
         'export_is_completed'           => 'Export is completed',
-        'update_catalog_to_xml_confirm' => 'Run export catalog?',
+        'update_catalog_to_xml_confirm' => 'Run catalog export?',
     ],
 ];
