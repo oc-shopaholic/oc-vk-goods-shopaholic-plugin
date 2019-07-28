585f850e9f57d07fe14f7869139f52bb4f84332a
Pavel Lovtsevich <p.lovtsevich@lovata.com>
2019-07-28 11:24:44 +0300

    New translations lang.php (Slovak)

--- lang/sk/lang.php
+++ lang/sk/lang.php
@@ -1,19 +1,19 @@
 <?php return [
     'plugin'     => [
         'name'        => 'VK Goods for Shopaholic',
-        'description' => 'Export catalog to VK Goods using API',
+        'description' => 'Allows to export products to VK Goods (vk.com)',
     ],
     'menu'       => [
-        'settings'             => 'Export to VK Goods',
-        'settings_description' => 'Configure catalog export to VK Goods',
+        'settings'             => 'VK Goods settings',
+        'settings_description' => 'Configure products export and queue management',
     ],
     'field'      => [
         'community_id'                               => 'Community ID',
-        'code_model_for_images'                      => 'Get images from:',
+        'code_model_for_images'                      => 'Get images from',
         'section_management_additional_fields_offer' => 'Additional fields',
         'section_vk_goods'                           => 'VK Goods',
         'active_vk'                                  => 'Export to VK Goods',
-        'access_token_user'                          => 'User access key',
+        'access_token_user'                          => 'API Access token',
         'category_vk_id'                             => 'VK Goods category',
         'queue_name'                                 => 'Queue name',
     ],
@@ -21,13 +21,13 @@
         'export_catalog_to_api' => 'Run export',
     ],
     'widget'     => [
-        'export_catalog_to_api_for_vk_goods' => 'Export catalog to VK Goods',
+        'export_catalog_to_api_for_vk_goods' => 'Export products to VK Goods',
     ],
     'permission' => [
-        'vkgoodssettings' => 'Manage settings of catalog export to VK Goods',
+        'vkgoodssettings' => 'Manage VK Goods settings',
     ],
     'message'    => [
-        'export_is_completed'           => 'Export is completed',
-        'update_catalog_to_xml_confirm' => 'Run catalog export?',
+        'export_is_completed'           => 'Export has completed successfully!',
+        'update_catalog_to_xml_confirm' => 'All products will be exported to your VK Shop. Continue?',
     ],
 ];
