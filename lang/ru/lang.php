4a22ef7d6a0554abed2e4bb20c365cae8db391ac
Pavel Lovtsevich <p.lovtsevich@lovata.com>
2019-07-28 11:24:43 +0300

    New translations lang.php (Russian)

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -1,33 +1,33 @@
 <?php return [
     'plugin'     => [
         'name'        => 'VK товары для Shopaholic',
-        'description' => 'Интеграция с API VK Goods для выгрузки каталога',
+        'description' => 'Позволяет экспортировать товары в VK Goods (vk.com)',
     ],
     'menu'       => [
-        'settings'             => 'Экспорт в Товары VK',
-        'settings_description' => 'Настроить экспорт каталога в Товары VK',
+        'settings'             => 'Настройки VK Goods',
+        'settings_description' => 'Настройка экспорта товаров и управление очередью',
     ],
     'field'      => [
         'community_id'                               => 'ID сообщества',
-        'code_model_for_images'                      => 'Получать изображения из:',
+        'code_model_for_images'                      => 'Получать изображения из',
         'section_management_additional_fields_offer' => 'Дополнительные поля',
         'section_vk_goods'                           => 'Товары VK',
-        'active_vk'                                  => 'Экспорт в Товары VK',
-        'access_token_user'                          => 'Ключ доступа пользователя',
+        'active_vk'                                  => 'Экспортировать в Товары VK',
+        'access_token_user'                          => 'Ключ доступа к API',
         'category_vk_id'                             => 'Категория VK Goods',
         'queue_name'                                 => 'Название очереди',
     ],
     'button'     => [
-        'export_catalog_to_api' => 'Начать экспортирование',
+        'export_catalog_to_api' => 'Запустить экспорт',
     ],
     'widget'     => [
-        'export_catalog_to_api_for_vk_goods' => 'Экспорт каталога в API (VK Goods)',
+        'export_catalog_to_api_for_vk_goods' => 'Экспорт товаров в VK Goods',
     ],
     'permission' => [
-        'vkgoodssettings' => 'Управление настройками экспорта в VK Goods',
+        'vkgoodssettings' => 'Управление настройками VK Goods',
     ],
     'message'    => [
-        'export_is_completed'           => 'Экспортирование начато',
-        'update_catalog_to_xml_confirm' => 'Начать экспортирование каталога?',
+        'export_is_completed'           => 'Экспорт успешно завершён!',
+        'update_catalog_to_xml_confirm' => 'Все товары будут экспортированы в ваш VK магазин. Продолжить?',
     ],
 ];
