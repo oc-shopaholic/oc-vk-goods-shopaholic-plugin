bf122ab0f701d81d6149a997955a02380c952c6a
Pavel Lovtsevich <p.lovtsevich@lovata.com>
2019-10-18 14:41:04 +0300

    New translations lang.php (Finnish)

---
+++ lang/fi/lang.php
@@ -0,0 +1,33 @@
+<?php return [
+    'plugin'     => [
+        'name'        => '',
+        'description' => '',
+    ],
+    'menu'       => [
+        'settings'             => '',
+        'settings_description' => '',
+    ],
+    'field'      => [
+        'community_id'                               => '',
+        'code_model_for_images'                      => '',
+        'section_management_additional_fields_offer' => '',
+        'section_vk_goods'                           => '',
+        'active_vk'                                  => '',
+        'access_token_user'                          => '',
+        'category_vk_id'                             => '',
+        'queue_name'                                 => '',
+    ],
+    'button'     => [
+        'export_catalog_to_api' => '',
+    ],
+    'widget'     => [
+        'export_catalog_to_api_for_vk_goods' => '',
+    ],
+    'permission' => [
+        'vkgoodssettings' => '',
+    ],
+    'message'    => [
+        'export_is_completed'           => '',
+        'update_catalog_to_xml_confirm' => '',
+    ],
+];
