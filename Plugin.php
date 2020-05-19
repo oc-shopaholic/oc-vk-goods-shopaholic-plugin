d844c06c8bb092189cc2d2e3a0d7648520f78c88
Nick Khaetsky <rzzsapb@gmail.com>
2020-05-19 16:40:25 +0300

    Add October Drivers in plugin dependencies

--- Plugin.php
+++ Plugin.php
@@ -25,7 +25,7 @@ use Lovata\VkGoodsShopaholic\Classes\Event\Category\ExtendCategoryFieldsHandler;
 class Plugin extends PluginBase
 {
     /** @var array Plugin dependencies */
-    public $require = ['Lovata.Shopaholic', 'Lovata.Toolbox'];
+    public $require = ['Lovata.Shopaholic', 'Lovata.Toolbox', 'October.Drivers'];
 
     /**
      * Register settings
